12036a6d00342251ea3c97f7b31917966f775557
Your Name <peterisracinskis@gmail.com>
2021-10-20 16:05:15 +0300

    KMB_1 change output, details

--- KMB_1/compute-mobius.py
+++ KMB_1/compute-mobius.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import sys
-INPUT_FILE="ieeja-ex2.txt"
+#INPUT_FILE="ieeja-ex1.txt" # change this to change input filename
+#INPUT_FILE="ieeja-ex2.txt" # change this to change input filename
+INPUT_FILE="ieeja.txt" # change this to change input filename
 
 # a smarter implementation might exist but this is at least polynomial
 def compute_mobius_cubic(ordering, n, output):
@@ -17,32 +19,32 @@ def compute_mobius_cubic(ordering, n, output):
 def ascending_order(array: list, n):
     return sorted(array[1:], key=lambda x: x.index(1))
 
-def transpose_and_clean(array, n):
+def transpose(array, n):
     return [[array[j][i] for j in range(n)] for i in range(n)]
 
 def print_array_line(line: list):
     output = ""
     for element in line:
         output = "{}{}\t".format(output, element)
-    output = "[{}]".format(output)
+    output = "[{}]".format(output[:-1])
     print(output)
 
 def process_input(array):
     n = array[0][0]
     output = [[0] * n for _ in range(n)]
     array = ascending_order(array, n)
-    transposed = transpose_and_clean(array, n)
+    transposed = transpose(array, n)
     compute_mobius_cubic(transposed, n, output)
     return output
 
 
 if __name__ == "__main__":
-    i, j = (int(x)-1 for x in sys.argv[1:])
+    i, j = (int(x)-1 for x in sys.argv[1:]) # minus 1 because mathematicians start indexing from 1
     array = []
     with open(INPUT_FILE, mode="r") as f:
         [array.append(
             [int(y) for y in filter(lambda z: z.isdigit(), list(x))])
             for x in filter(lambda x: x[0] != "#", f.readlines())]
     output = process_input(array)
-    [print_array_line(line) for line in output]
-    print("[{},{}] {}".format(i+1, j+1, output[j][i])) #inverted because indices are easier to work with that way
+    #[print_array_line(line) for line in output] # for printing the array (not in homework requirement)
+    print("[{},{}] {}".format(i+1, j+1, output[j][i]))
