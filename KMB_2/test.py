439722f90b3b1ca0fa054b8233244971567a1be8
Your Name <peterisracinskis@gmail.com>
2021-11-11 15:59:59 +0200

    KMB_2 fix index

--- KMB_2/test.py
+++ KMB_2/test.py
@@ -1,7 +1,7 @@
 from debruijn import *
 
 if __name__ == "__main__":
-    for m in range(10):
+    for m in range(11):
         for n in range(5):
             print("m = {}, n = {}".format(m,n))
             result = find_debruijn(m, n)
