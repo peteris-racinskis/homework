86b9001084d7bf91fcad45a3d4c6b32e822cfea3
peteris racinskis <peterisracinskis@gmail.com>
2021-12-19 22:58:13 +0200

    ST_MD2 done (first pass, will check tomorrow)

--- ST_MD2/datagen.py
+++ ST_MD2/datagen.py
@@ -1,6 +1,3 @@
-from typing import List, Tuple
-
-
 def fac(x):
     fac = {1:1, 2:2, 3:6}
     return fac[x]
@@ -15,12 +12,6 @@ def pareto_dominated(x, y):
             smaller = True
     return bigger and not smaller
 
-def pareto_next(x_list, y):
-    for x in x_list:
-        if pareto_dominated(x, y):
-            return True
-    return False
-
 def pareto_top(x_list, y):
     for x in x_list:
         if pareto_dominated(y, x):
@@ -28,32 +19,29 @@ def pareto_top(x_list, y):
             return False
     return True
 
-frame = []
-cells = []
-for z in range(1,4):
-    print(f"z = {z}")
-    matrix = []
-    for x in range(1,4):
-        row = []
-        for y in range(1,4):
-            p1 = x * (x - fac(y) - 0.5) 
-            p2 = y * (y - fac(z))
-            p3 = z * (z - fac(x))
-            row.append((p1, p2, p3))
-            cells.append((p1, p2, p3))
-        s = " ".join([f"{i}\t" for i in row])
-        matrix.append(row)
-        print(s)
-    frame.append(matrix)
-
-print()
-print("Pareto optima")
-top = []
-for cell in cells:
-    if pareto_top(cells, cell):
-        top.append(cell)
-print("remaining:")
-[print(t) for t in top]
-print("verify - will print if any dominates")
-for cell in top:
-    pareto_top(top, cell)
+if __name__ == "__main__":
+    frame = []
+    cells = []
+    for z in range(1,4):
+        print(f"z = {z}")
+        matrix = []
+        for x in range(1,4):
+            row = []
+            for y in range(1,4):
+                p1 = x * (x - fac(y) - 0.5) 
+                p2 = y * (y - fac(z))
+                p3 = z * (z - fac(x))
+                row.append((p1, p2, p3))
+                cells.append((p1, p2, p3))
+            s = " ".join([f"{i}\t" for i in row])
+            matrix.append(row)
+            print(s)
+        frame.append(matrix)
+    print()
+    print("Pareto optima")
+    top = []
+    for cell in cells:
+        if pareto_top(cells, cell):
+            top.append(cell)
+    print("remaining:")
+    [print(t) for t in top]
