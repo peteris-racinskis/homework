6ebbd483d96b3b5963980a40d4174a0f072835e4
peteris racinskis <peterisracinskis@gmail.com>
2021-12-22 00:41:28 +0200

    DIA_MD script for md2-5

---
+++ DIA_MD/elbow.py
@@ -0,0 +1,25 @@
+import sys
+IFILE=sys.argv[1]
+
+def curve(x1, x2, x3):
+    prev = x2-x1
+    nex = x3-x2
+    return prev / nex
+
+def curve_max(x_list):
+    max = 0
+    amax = 0
+    for i in range(1, len(x_list)-1) :
+        x1, x2, x3 = x_list[i-1:i+2]
+        c = curve(x1, x2, x3)
+        if i > 1 and not abs(c) > max:
+            break
+        amax = (i,x2) if abs(c) > max else amax
+        max = abs(c) if abs(c) > max else max
+        print(f"x = {x2} curve = {c}")
+    print(f"max = {max}")
+    print(f"amax = {amax}")
+
+with open(IFILE, 'r') as f:
+    xlist = [float(x) for x in f.readlines()]
+curve_max(xlist)
