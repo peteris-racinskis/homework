b3618101e6bc85b2f43bab8f567007d864f38aaa
peteris racinskis <peterisracinskis@gmail.com>
2022-01-11 15:34:24 +0200

    GT4 fix err

--- DIA_MD/md2-1-1.R
+++ DIA_MD/md2-1-1.R
@@ -1,9 +1,10 @@
-dist.de <- read.table("wg22_dist.txt")
+dist.de <- read.table("Results-1980-2015-dist.txt")
+names <- read.table("Results-1980-2015-name.txt")
 fit <- cmdscale(dist.de, eig = TRUE, k = 2)
 x <- fit$points[, 1]
 y <- fit$points[, 2]
-plot(x,y,xlim=c(-150,150),ylim=c(-120,120))
-text(x,y,labels=dist.names$V1)
+plot(x,y, pch=4, col="grey", xlim=c(-1.5,2.5))
+text(x,y,labels=names$V1)
 x <- -x
 y <- -y
 plot(y,x,xlim=c(-120,120),ylim=c(-150,150))
